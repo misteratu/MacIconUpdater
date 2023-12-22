3e3d351a424e88cf962937a94c2a92f224cad2f1
Picard Arthur <misteratu8@gmail.com>
2023-12-22 18:28:16 +0100

    update

--- IconScriptSetup.sh
+++ IconScriptSetup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Demander à l'utilisateur et attendre une réponse y/n
+while true; do
+    read -p "Voulez-vous installer ? (y/n) " reponse
+    case $reponse in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Veuillez répondre par y ou n.";;
+    esac
+done
+
 # Nom de l'utilisateur
 username=$(whoami)
 
@@ -12,5 +22,22 @@ else
     # Rajout d'un dossier afin de stocker les icons
     echo "Ajout d'un dossier icons pour stocker les icons"
     echo "$icons_folder"
-    mkdir "icons" "/Users/$username/opt"
+
+    # A vérifier la syntaxe de mkdir
+    mkdir "/Users/$username/opt/icons"
 fi
+
+chmod +x IconScriptUpdate.sh
+./IconScriptUpdate.sh
+
+# Demander à l'utilisateur et attendre une réponse y/n
+while true; do
+    read -p "Voulez-vous ouvrir le script à chaque démarrage ? (y/n) " reponse
+    case $reponse in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Veuillez répondre par y ou n.";;
+    esac
+done
+
+#launchctl load -w /Library/LaunchDaemons/com.IconScriptUpdate.plist
